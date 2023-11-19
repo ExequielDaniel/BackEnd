58651ac72db57038c885b3feda6059b7648ea258
ExequielDaniel <130413878+ExequielDaniel@users.noreply.github.com>
2023-11-18 21:48:46 -0300

    Add files via upload

--- sumativa 2/blog/models.py
+++ sumativa 2/blog/models.py
@@ -1,28 +1,33 @@
-# blog/models.py
 
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length = 200)
+
 
     def __str__(self):
         return self.nombre
 
 class Hashtag(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length = 200)
+
 
     def __str__(self):
         return self.nombre
 
 class Post(models.Model):
-    titulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length = 200)
     texto = models.TextField()
-    fecha = models.DateTimeField(default=timezone.now)
-    autor = models.ForeignKey(User, on_delete=models.CASCADE)
-    categoria = models.ForeignKey(Category, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(default = timezone.now)
+    autor = models.ForeignKey(User, on_delete = models.CASCADE)
+    categoria = models.ForeignKey(Category, on_delete = models.CASCADE)
     etiquetas = models.ManyToManyField(Hashtag)
 
-    def __str__(self):
-        return self.titulo + " (" + self.autor.first_name + " " + self.autor.last_name + ")"
+
+def __str__(self):
+    return (
+        f"{self.titulo} "
+        f"({self.autor.first_name} {self.autor.last_name})"
+ )
